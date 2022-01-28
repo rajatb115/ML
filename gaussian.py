48d3288f877d12474b018ab426a7cf585d1d09ce
Rajat Singh <rajatsingh.b115@gmail.com>
2022-01-29 00:45:22 +0530

    code improvement

--- gaussian.py
+++ gaussian.py
@@ -8,6 +8,7 @@ import tensorflow_probability as tfp
 def find_gaussian_parameters(data, K, n_iterations):
     # Here data is a one dimensional list
     samples = data.shape[0]
+    print(data.shape)
 
     # Initial guess of the parameters
     mus = np.random.rand(K)
@@ -17,18 +18,47 @@ def find_gaussian_parameters(data, K, n_iterations):
     # Train the model
     for _ in tqdm(range(n_iterations), desc="Training for each pixel"):
         # E step
-        resp = np.zeros((samples, K))
-        for i in range(samples):
-            for j in range(K):
-                resp = class_probabilities * tfp.distributions.Normal(loc=mus[j], scale=sigmas[j]).prob(data[i])
+        #resp = np.zeros((samples, K))
+        #for i in range(samples):
+        #    for j in range(K):
+        #        resp[i, j] = class_probabilities[j] * tfp.distributions.Normal(loc=mus[j], scale=sigmas[j]).prob(data[i])
+
+        resp = tfp.distributions.Normal(loc=mus, scale=sigmas).prob(data.reshape(-1, 1)).numpy() * class_probabilities
 
         resp /= np.linalg.norm(resp, axis=1, ord=1, keepdims=True)
         class_resp = np.sum(resp, axis=0)
 
         # M step
         for i in range(K):
-            class_probabilities[i] = class_probabilities[i]/samples
-            mus[i] = np.sum(resp[:, i]*data)
-            sigmas[i] = np.sqrt(np.sum(resp[:, i] * (data - mus[i])**2) / class_resp[i])
+            class_probabilities[i] = class_resp[i]/samples
+            mus[i] = np.sum(resp[:, i] * data) / class_resp[i]
+            sigmas[i] = np.sqrt(
+                np.sum(resp[:, i] * (data - mus[i])**2) / class_resp[i]
+            )
 
     return class_probabilities, mus, sigmas
+
+# Testing the function
+class_probs_true = [0.6, 0.4]
+mus_true = [2.5, 4.8]
+sigmas_true = [0.6, 0.3]
+random_seed = 42
+n_samples = 1000
+n_iterations = 100
+n_classes = 2
+
+# generate the data
+univariate_gmm = tfp.distributions.MixtureSameFamily(
+        mixture_distribution=tfp.distributions.Categorical(probs=class_probs_true),
+        components_distribution=tfp.distributions.Normal(
+            loc=mus_true,
+            scale=sigmas_true,
+        )
+    )
+
+dataset = univariate_gmm.sample(n_samples, seed=random_seed).numpy()
+class_probs, mus, sigmas = find_gaussian_parameters(dataset, n_classes, n_iterations)
+
+print(class_probs)
+print(mus)
+print(sigmas)
--- main.py
+++ main.py
@@ -5,6 +5,25 @@ import cv2
 import numpy as np
 from gaussian import find_gaussian_parameters
 
+def process_data(i,t,np_frame):
+    data_frames = np_frame[max(0, i-t): i]
+    frame_shape = data_frames[0].shape
+
+    frame_pixel = []
+    for row in range(frame_shape[0]):
+        tmp = []
+        for col in range(frame_shape[1]):
+            tmp.append([])
+
+        frame_pixel.append(tmp)
+
+    #print(len(frame_pixel[0]))
+    for _ in range(len(data_frames)):
+        for row in range(frame_shape[0]):
+            for col in range(frame_shape[1]):
+                frame_pixel[row][col].append(data_frames[_][row][col])
+
+    return frame_pixel
 # Reading video
 read_video = cv2.VideoCapture('data/input_video.mpg')
 
@@ -36,14 +55,26 @@ print("# Number of frames in the video = ", len(np_frame))
 # Adding animation for loading
 K = 3
 n_iterations = 10
-for i in tqdm(range(int(len(np_frame))), desc="Processing data"):
-    # Process each frame
-    data = []
-    class_probabilities, mus, sigmas = find_gaussian_parameters(data, K, n_iterations)
-    print(class_probabilities)
-    print(mus)
-    print(sigmas)
-    pass
+t = 10
+
+for i in tqdm(range(4,int(len(np_frame))), desc="Processing data"):
+    # Process each frame for time t
+
+    data_lst = process_data(i+1, t, np_frame)
+
+    for __ in range(len(data_lst)):
+        for _ in range(len(data_lst[0])):
+            final_lst = []
+            for j in range(min(t, i+1)):
+                final_lst.append(data_lst[__][_][j][0])
+
+            data = np.array(final_lst)
+
+            class_probabilities, mus, sigmas = find_gaussian_parameters(data, K, n_iterations)
+            print(class_probabilities)
+            print(mus)
+            print(sigmas)
+    break
 
 # Closing the VideoCapture object
 read_video.release()
